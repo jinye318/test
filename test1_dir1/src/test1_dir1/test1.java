d5160577691de987f1eb3c338449a94de6a6b298
jinye318 <jinye318@naver.com>
2015-03-19 16:43:43 +0900

    test1

---
+++ test1_dir1/src/test1_dir1/test1.java
@@ -0,0 +1,10 @@
+package test1_dir1;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("It is test in dir1");
+	}
+
+}
---
+++ test2_dir2/src/test2_dir2/test2.java
@@ -0,0 +1,10 @@
+package test2_dir2;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is test in dir2");
+	}
+
+}
