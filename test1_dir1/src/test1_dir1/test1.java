1a56383608406d83eb720956b2987d27ba81936b
jinye318 <jinye318@naver.com>
2015-03-19 16:45:46 +0900

    test2

--- test1_dir1/src/test1_dir1/test1.java
+++ test1_dir1/src/test1_dir1/test1.java
@@ -5,6 +5,7 @@ public class test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("It is test in dir1");
+		System.out.println("second try");
 	}
 
 }
